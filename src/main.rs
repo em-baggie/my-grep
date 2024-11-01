058e611ddbf108e4dfed1e57c87094f22b64f720
Emma <156218556+em-baggie@users.noreply.github.com>
2024-11-01 17:11:02 +0000

    more changes

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,32 @@ use std::fs;
 fn main() {
     // read any CL arguments passed to it and collects them into a vector
     let args: Vec<String> = env::args().collect();
-    // first argument is path of binary file (like in C)
-    // save values of two CL arguments
-
-    let query = &args[1];
-    let file_path = &args[2];
-
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    let config = parse_config(&args);
 
     // read contents of file, will panic if error occurs or return Ok value
     // file is automatically closed after reading
-    let contents = fs::read_to_string(file_path)
-        .expect("should have been able to read the file");
+    let contents = fs::read_to_string(config.file_path).expect("should have been able to read the file");
 
     println!("With text:\n{contents}");
 }
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+// 
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    // cloning ensures that the struct has ownership of the data
+    Config { 
+        query,
+        file_path,
+    }
+
+}
+
+
 
