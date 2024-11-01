448c3e2c66240ac924c31a16e07e583b2b9857f1
Emma <156218556+em-baggie@users.noreply.github.com>
2024-11-01 09:37:36 +0000

    started main function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-fn main() {
-    // below allows pogram to read any CL arguments passed to it
+use std::env;
 
-    
+fn main() {
+    // below allows pogram to read any CL arguments passed to it and collects them into a vector
+    let args: Vec<String> = env::args().collect();
+    dbg!(args);
 }
+
