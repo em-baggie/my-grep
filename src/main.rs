03a846b0d7fd6867ef3905da0490540d29a90a55
Emma <156218556+em-baggie@users.noreply.github.com>
2024-11-01 10:31:50 +0000

    continuing main function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
 use std::env;
+use std::fs;
 
 fn main() {
-    // below allows pogram to read any CL arguments passed to it and collects them into a vector
+    // read any CL arguments passed to it and collects them into a vector
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    // first argument is path of binary file (like in C)
+    // save values of two CL arguments
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {query}");
+    println!("In file {file_path}");
+
+    // read contents of file, will panic if error occurs or return Ok value
+    // file is automatically closed after reading
+    let contents = fs::read_to_string(file_path)
+        .expect("should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
 
