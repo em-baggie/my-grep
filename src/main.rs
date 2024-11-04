77beca2a269739244a6b25797069f5a86d2e7eb2
Emma <156218556+em-baggie@users.noreply.github.com>
2024-11-04 18:39:21 +0000

    split into main and lib

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,30 @@
-use std::env;
-use std::fs;
+use std:: {env, fs, process, error::Error};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
+    // better than unwrap_or_else as do not need to access the Ok value
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 
-    // --snip--
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n{contents}");
+
+    Ok(())
 }
 
 struct Config {
@@ -20,12 +32,14 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    Config {
-        query,
-        file_path,
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+    
+        Ok(Config { query, file_path })
     }
 }
