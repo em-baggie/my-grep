a28912389b6b202ecb0ec30e93403832f60eefbd
Emma <156218556+em-baggie@users.noreply.github.com>
2024-11-04 18:49:06 +0000

    finished splitting into lib/main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std:: {env, fs, process, error::Error};
+use std:: {env, process};
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,34 +13,9 @@ fn main() {
     println!("In file {}", config.file_path);
 
     // better than unwrap_or_else as do not need to access the Ok value
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }
 
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-    
-        Ok(Config { query, file_path })
-    }
 }
